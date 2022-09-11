ca6877431006d74ce399e1aa9c6906d248852aae
AresKarry <105958567+AresKarry@users.noreply.github.com>
2022-09-11 15:13:26 +0800

    Add files via upload

---
+++ Vue_test/01_src_分析脚手架/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <img src="./assets/logo.png" alt="logo">
+    <StarKai></StarKai>
+    <FilmsKai></FilmsKai>
+  </div>
+</template>
+
+<script>
+//
+  import StarKai from './components/StarKai.vue'
+  import FilmsKai from './components/FilmsKai.vue'
+  export default {
+  components: { FilmsKai, StarKai },
+    name:'App',
+    template:`<App></App>`,
+    component:{
+      StarKai,
+      FilmsKai
+    }
+
+  }
+  
+</script>
+
+<style>
+
+</style>
---
+++ Vue_test/01_src_分析脚手架/components/StarKai.vue
@@ -0,0 +1,32 @@
+<template>
+  <!-- 组件的结构 -->
+  <div class="box">
+    <h2>演员姓名：{{actorName}}</h2>
+    <h2>演员年龄：{{actorAge}}</h2>
+    <h2>粉丝名称：{{actorFans}}</h2>
+  </div>
+</template>
+
+<script>
+// 组件交互相关的代码（数据、方法……）
+  export default {
+    name:'StarKai',
+    data(){
+      return {
+          actorName:'王俊凯',
+          actorAge:22,
+          actorFans:'小螃蟹'        
+      }
+    },
+    methods:{
+
+    }
+  }
+</script>
+
+<style>
+/* 组件的样式 */
+  .box{
+    background-color: pink;
+  }
+</style>
