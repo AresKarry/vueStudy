9b8fad7d0779086e82fdfe97119b90a561983a10
AresKarry <105958567+AresKarry@users.noreply.github.com>
2022-09-11 15:28:13 +0800

    Add files via upload

---
+++ Vue_test/src/main.js
@@ -0,0 +1,33 @@
+//引入vue
+import Vue from 'vue'
+//引入APP
+import App from './App.vue'
+
+// 完整引入
+// 引入ElementUI组件库
+// import ElementUI from 'element-ui';
+// 引入ElementUI全部样式
+// import 'element-ui/lib/theme-chalk/index.css';
+
+// 按需引入
+import { Button, Row , DatePicker } from 'element-ui';
+
+//关闭vue的生产提示
+Vue.config.productionTip=false
+// 应用ElementUI
+// Vue.use(ElementUI);
+
+Vue.component("el-button", Button);
+Vue.component("el-row", Row);
+Vue.component("el-date-picker", DatePicker);
+
+/* 或写为
+ * Vue.use(Button)
+ * Vue.use(Select)
+ */
+
+//创建vm
+new Vue({
+    render: h => h(App),
+    
+}).$mount('#app')
